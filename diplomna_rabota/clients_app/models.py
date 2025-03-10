d4621ce1cc4481dfd7fa71d14aac146b2ddf6f2a
Pavel Kalchishkov <117100876+Pavcho@users.noreply.github.com>
2025-03-10 15:59:57 +0200

    added clients app
    added clients app with a single ''Client'' model

---
+++ diplomna_rabota/clients_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from .validators import AllNumbersValidator, TenCharactersValidator
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100,
+                                    validators=[AllNumbersValidator,TenCharactersValidator])
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
+    orders = ArrayField(models.IntegerField(), blank=True, default=list) # list of the order id's
+
---
+++ diplomna_rabota/clients_app/validators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+import re
+
+
+@deconstructible
+class AllNumbersValidator:
+    def __call__(self, value: str, *args, **kwargs):
+        if not re.fullmatch(r'[0-9]+', value):
+            raise ValidationError("Incorrect phone number!")
+
+
+@deconstructible
+class TenCharactersValidator:
+    def __call__(self, value: str, *args, **kwargs):
+        if len(value) != 10:
+            raise ValidationError("Your phone number must be 10 digits!")
