2df71a94ff867812b6c5ef62f52786a34e092ddb
Pavel Kalchishkov <117100876+Pavcho@users.noreply.github.com>
2025-03-21 14:14:08 +0200

    added a common folder and added phone number to customuser
    added a common folder for utilities and validators and added phone number to customuser

--- diplomna_rabota/users_app/models.py
+++ diplomna_rabota/users_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from common.validators import AllNumbersValidator, TenCharactersValidator
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=10,
@@ -8,3 +9,7 @@ class CustomUser(AbstractUser):
                                     'unique': "A user with that username already exists.",
                                     'max_length': "The user's username must be 10 characters or less.",
                                 })
+
+    phone_number = models.CharField(max_length=100,
+                                    validators=[AllNumbersValidator,
+                                                TenCharactersValidator])
