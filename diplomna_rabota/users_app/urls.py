52897af242605cfa197e6ad68c6978fec704f486
Pavel Kalchishkov <117100876+Pavcho@users.noreply.github.com>
2025-03-21 14:02:54 +0200

    created profile details page
    created profile details html file and django view, and added them to the urls

--- diplomna_rabota/users_app/urls.py
+++ diplomna_rabota/users_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, logout_view
+from .views import UserRegisterView, UserLoginView, logout_view, UserProfileDetailsViews
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path('profile_details/', UserProfileDetailsViews.as_view(), name='profile_details'),
 ]
--- diplomna_rabota/users_app/views.py
+++ diplomna_rabota/users_app/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.views import LoginView
 from .forms import CustomUserCreationForm, CustomUserLoginForm
 from django.urls import reverse_lazy
+from .models import CustomUser
 
 
 class UserRegisterView(CreateView):
@@ -19,3 +21,23 @@ class UserLoginView(LoginView):
 def logout_view(request):
     logout(request)
     return render(request, 'user/logout.html')
+
+
+class UserProfileDetailsViews(LoginRequiredMixin, DetailView):
+    model = CustomUser
+    pk_url_kwarg = 'pk'
+    template_name = 'user/profile_details.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context['username'] = user.username
+        context['first_name'] = user.first_name
+        context['last_name'] = user.last_name
+        context['email'] = user.email
+
+        return context
