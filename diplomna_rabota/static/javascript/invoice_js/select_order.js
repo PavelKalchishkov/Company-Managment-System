8b97959360a1e63c8923b68fef42345cddd55ac5
Pavel Kalchishkov <117100876+Pavcho@users.noreply.github.com>
2025-05-27 16:08:51 +0300

    changed it so the javascript works with jsquery(needed for select2)
    changed it so the javascript works with jsquery(needed for select2)

--- diplomna_rabota/static/javascript/invoice_js/select_order.js
+++ diplomna_rabota/static/javascript/invoice_js/select_order.js
@@ -1,12 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-const orderSelect = document.getElementById("id_order");
-const price_without_dds_input = document.getElementById("id_whole_price_without_dds");
-const price_with_dds_input = document.getElementById("id_whole_price_with_dds")
-const dds = document.getElementById("id_DDS")
-const discount = document.getElementById("id_discount")
+$(document).ready(function () {
+  const price_without_dds_input = $("#id_whole_price_without_dds");
+  const price_with_dds_input = $("#id_whole_price_with_dds");
+  const dds = $("#id_DDS");
+  const discount = $("#id_discount");
 
 function update_price() {
-  const orderId = orderSelect.value;
+  const orderId = $("#id_order").val();
   if (!orderId)
       return;
 
@@ -16,13 +15,13 @@ function update_price() {
     })
     .then(data => {
       if (data.price !== undefined) {
-        let dds_value = parseFloat(dds.value);
+        let dds_value = parseFloat(dds.val());
         if (dds_value !== 0) {
           dds_value *= 0.01;
           dds_value *= data.price;
         }
 
-        let discount_value = parseFloat(discount.value);
+        let discount_value = parseFloat(discount.val());
         if (discount_value !== 0 && discount_value) {
             discount_value *= 0.01;
         }
@@ -30,8 +29,8 @@ function update_price() {
           discount_value = 0
         }
 
-          price_without_dds_input.value = (data.price - (data.price * discount_value)).toFixed(2);
-          price_with_dds_input.value = ((data.price + dds_value) - (data.price + dds_value) * discount_value).toFixed(2);
+          price_without_dds_input.val((data.price - (data.price * discount_value)).toFixed(2));
+          price_with_dds_input.val(((data.price + dds_value) - (data.price + dds_value) * discount_value).toFixed(2));
       }
     })
     .catch(error => {
@@ -39,20 +38,8 @@ function update_price() {
     });
   }
 
-orderSelect.addEventListener("change", () => {
-  update_price()
-
+$("#id_order").on("change", update_price);
+$("#id_discount").on("input", update_price);
+$("#id_DDS").on("change", update_price);
 });
 
-discount.addEventListener("input", () => {
-  if (orderSelect.value) {
-    update_price()
-  }
-});
-
-dds.addEventListener("change", () => {
-  if (orderSelect.value) {
-    update_price()
-  }
-})
-});
