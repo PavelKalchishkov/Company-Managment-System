57873e60fe81db5beb9265e8c66f30a5f8ccfc17
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-08-06 17:36:34 +0300

    removed the universal details view css file from the clients css folder
    removed the universal details view css file from the clients css folder

--- diplomna_rabota/vendors_app/models.py
+++ diplomna_rabota/vendors_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from common.validators import AllNumbersValidator, TenCharactersValidator
 
 
-
 class Vendor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
