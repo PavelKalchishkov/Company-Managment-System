de9c41c65ebc5d3c184ec6b0706ba67ed3cc062f
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-08-12 16:20:22 +0300

    shippers app now require permission from admin
    shippers app now require permission from admin

--- Company Management System/shippers_app/views.py
+++ Company Management System/shippers_app/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -6,12 +6,13 @@ from .forms import ShipperCreationForm, ShipperUpdateForm
 from .models import Shipper
 
 
-class ShippersView(LoginRequiredMixin, ListView):
+class ShippersView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Shipper
     template_name = 'table_views/shippers/shippers.html'
     context_object_name = 'shippers'
-
     ordering = ['-id']
+
+    permission_required = 'shippers_app.view_shipper'
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -44,29 +45,37 @@ class ShippersView(LoginRequiredMixin, ListView):
         context['user'] = user
         return context
 
-class ShippersAddView(LoginRequiredMixin, CreateView):
+class ShippersAddView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Shipper
     form_class = ShipperCreationForm
     template_name = 'table_views/shippers/shippers_add.html'
     success_url = reverse_lazy('shippers_view')
 
-class ShippersUpdateView(LoginRequiredMixin, UpdateView):
+    permission_required = 'shippers_app.add_shipper'
+
+class ShippersUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Shipper
     form_class = ShipperUpdateForm
     pk_url_kwarg = 'pk'
     template_name = 'table_views/shippers/shippers_update.html'
     success_url = reverse_lazy('shippers_view')
 
-class ShippersDeleteView(LoginRequiredMixin, DeleteView):
+    permission_required = 'shippers_app.change_shipper'
+
+class ShippersDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Shipper
     pk_url_kwarg = 'pk'
     template_name = 'table_views/shippers/shippers_delete.html'
     success_url = reverse_lazy('shippers_view')
 
-class ShippersDetailView(LoginRequiredMixin, DetailView):
+    permission_required = 'shippers_app.delete_shipper'
+
+class ShippersDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Shipper
     pk_url_kwarg = 'pk'
     template_name = 'table_views/shippers/shippers_details.html'
+
+    permission_required = 'shippers_app.view_shipper'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
