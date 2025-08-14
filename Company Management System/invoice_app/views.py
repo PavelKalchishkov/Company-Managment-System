cb208eb26bec62accc312752be37075d04064278
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-08-14 18:29:17 +0300

    invoice now can be exported to pdf with a clean look
    invoice now can be exported to pdf with a clean look

--- Company Management System/invoice_app/views.py
+++ Company Management System/invoice_app/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
@@ -9,9 +11,12 @@ from .forms import CompanyCreateForm, CompanyUpdateForm, InvoiceCreateForm, Invo
 from .models import Company, Invoice
 from orders_app.models import OrderProduct, Order
 
+from django.http import FileResponse, HttpResponse
 import io
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 
 class CompaniesView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
@@ -215,16 +220,155 @@ class InvoicesReportView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
 
 
 def generate_pdf(request, invoice_id):
-    buffer = io.BytesIO()
     invoice = Invoice.objects.get(pk=invoice_id)
-    p = canvas.Canvas(buffer)
+    order_products = OrderProduct.objects.all()
 
-    p.drawString(15,15, f"{invoice}")
+    buffer = io.BytesIO()
+    doc = SimpleDocTemplate(buffer, pagesize=A4, rightMargin=30, leftMargin=30, topMargin=30, bottomMargin=18)
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name="CenterTitle", alignment=1, fontSize=26, spaceAfter=24))
+    styles.add(ParagraphStyle(name="CenterTitle2", alignment=1, fontSize=16, spaceAfter=16))
+    styles.add(ParagraphStyle(name="CenterTitle3", alignment=1, fontSize=12, spaceAfter=4))
+    styles.add(ParagraphStyle(name="CenterTitle4", alignment=1, fontSize=12, spaceAfter=16))
 
-    p.showPage()
-    p.save()
+    elements = []
+
+    elements.append(Paragraph("Invoice", styles["CenterTitle"]))
+    elements.append(Paragraph(f"{invoice}", styles["CenterTitle2"]))
+    elements.append(Paragraph(f"Date of issue", styles["CenterTitle3"]))
+    elements.append(Paragraph(f"{invoice.date}", styles["CenterTitle4"]))
+
+    # Supplier and Recipient Info
+    recipient_company_info = [
+        ["Recipient", invoice.company.recipient],
+        ["Company", invoice.company.name],
+        ["EIK", invoice.company.eik],
+        ["Address", Paragraph(invoice.company.address, styles["Normal"])],
+        ["MOL", invoice.company.mol],
+        ["DDS", invoice.company.dds],
+    ]
+
+    supplier_company_info = [
+        ["Supplier", "Pavel Kalchishkov"],
+        ["Company", "Technocenter OOD"],
+        ["EIK", "1111111111"],
+        ["Address", "Goce Delchev 30"],
+        ["Mol", "Pavel Kalchishkov"],
+        ["DDS", "BG111111111"]
+    ]
+
+    recipient_company_table = Table(recipient_company_info, colWidths=[100, 150])
+    recipient_company_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+    ]))
+
+    supplier_company_table = Table(supplier_company_info, colWidths=[100, 150])
+    supplier_company_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+    ]))
+
+    info_table = Table([
+        [recipient_company_table, supplier_company_table]
+    ])
+    elements.append(info_table)
+    elements.append(Spacer(1, 12))
+
+    order_details = [
+        ["Order", invoice.order],
+        ["Raw Price", invoice.order.order_price],
+        ["Payment Method", invoice.order.payment_method],
+        ["Status", invoice.order.order_status],
+        ["Client", invoice.order.client],
+        ["Employee", invoice.order.employee],
+        ["Shipper", invoice.order.shipper],
+    ]
+
+    invoice_details = [
+        ["Invoice", invoice],
+        ["DDS(%)", invoice.DDS],
+        ["Date", invoice.date],
+        ["Discount", invoice.discount],
+        ["Status", "Active" if not invoice.cancelled else "Cancelled"],
+        ["-","-"],
+        ["-", "-"]
+    ]
+
+    order_details_table = Table(order_details, colWidths=[100, 150])
+    invoice_details_table = Table(invoice_details, colWidths=[100, 150])
+
+    order_details_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+    ]))
+
+    invoice_details_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+    ]))
+
+    info_table_2 = Table([
+        [order_details_table, invoice_details_table],
+    ])
+    elements.append(info_table_2)
+    elements.append(Spacer(1, 12))
+
+    # Products Table
+    product_data = [["Products", "Quantity", "Unit Price", "Total"]]
+
+    for order_product in order_products:
+        if invoice.order.id == order_product.order_id:
+            product_data.append([order_product.product.name, order_product.quantity, order_product.product.price, order_product.product.price * order_product.quantity])
+
+    products_table = Table(product_data, colWidths=[255, 84, 86, 86])
+    products_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+        ("ALIGN", (1, 1), (-1, -1), "CENTER"),
+    ]))
+    elements.append(products_table)
+    elements.append(Spacer(1, 12))
+
+    # Totals
+    discount_no_dds = invoice.whole_price_without_dds * invoice.discount * Decimal(0.01)
+    discount_with_dds = invoice.whole_price_with_dds * invoice.discount * Decimal(0.01)
+
+
+    totals_data = [
+        ["Price", "Leva"],
+        ["Whole Price (No DDS)", f"{invoice.whole_price_without_dds:.2f} lv"],
+        ["Whole Price (With DDS)", f"{invoice.whole_price_with_dds:.2f} lv"],
+        ["Discount (No DDS)", f"{discount_no_dds:.2f} lv"],
+        ["Discount (With DDS)", f"{discount_with_dds:.2f} lv"],
+    ]
+    totals_table = Table(totals_data, colWidths=[255, 255])
+    totals_table.setStyle(TableStyle([
+        ("GRID", (0, 0), (-1, -1), 0.5, colors.black),
+        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
+    ]))
+    elements.append(totals_table)
+    elements.append(Spacer(1, 50))
+
+    signature_data = [
+        [
+            Paragraph("Received the invoice:<br/>Signature: ____________", styles["Normal"]),
+            "",  # Empty column for spacing
+            Paragraph("Issued the invoice:<br/>Signature: ____________", styles["Normal"])
+        ]
+    ]
+
+    signature_table = Table(signature_data, colWidths=[200, 70, 200], hAlign="CENTER")
+    signature_table.setStyle(TableStyle([
+        ("TOPPADDING", (0, 0), (-1, -1), 30),  # space above
+        ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+    ]))
+    elements.append(signature_table)
+
+
+    doc.build(elements)
 
     buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename="invoice.pdf")
+    return FileResponse(buffer, as_attachment=True, filename=f"invoice_{invoice}.pdf")
 
 
