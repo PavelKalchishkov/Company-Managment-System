3d0888292cc800580a196f36771d6edb5901426b
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-08-15 16:16:34 +0300

    added change password option for users
    added change password option for users

--- Company Management System/users_app/urls.py
+++ Company Management System/users_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserRegisterView, UserLoginView, logout_view, UserProfileDetailsViews, UserProfileUpdateView, \
-    UserProfileDeleteView
+    UserProfileDeleteView, change_password, change_password_done
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('profile_details/', UserProfileDetailsViews.as_view(), name='profile_details'),
     path('profile_edit/', UserProfileUpdateView.as_view(), name='profile_edit'),
     path('profile_delete/', UserProfileDeleteView.as_view(), name='profile_delete'),
+    path('password_change/', change_password, name='password_change'),
+    path('password_change/done/', change_password_done, name='password_change_done'),
 ]
--- Company Management System/users_app/views.py
+++ Company Management System/users_app/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from .forms import CustomUserCreationForm, CustomUserLoginForm, CustomUserEditProfileForm
 from django.urls import reverse_lazy
 from .models import CustomUser
@@ -62,3 +64,19 @@ class UserProfileDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            update_session_auth_hash(request, user)
+            return redirect('password_change_done')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'user/password_change.html', {'form': form})
+
+def change_password_done(request):
+    return render(request, 'user/password_change_done.html')
