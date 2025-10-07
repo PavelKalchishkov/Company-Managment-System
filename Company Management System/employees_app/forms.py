97eddbb2ca1481967079482fc825f12e15fd8ce7
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-10-07 18:00:15 +0300

    translation of employees done
    translation of employees done

--- Company Management System/employees_app/forms.py
+++ Company Management System/employees_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-
 from .models import Employee
+from django.utils.translation import gettext_lazy as _
+
 
 class EmployeeCreateForm(forms.ModelForm):
     class Meta:
@@ -8,15 +9,20 @@ class EmployeeCreateForm(forms.ModelForm):
         fields = '__all__'
 
         labels = {
-            'personal_phone_number': 'Personal phone number (optional)',
-            'address': 'Address (optional)',
-            'city': 'City (optional)',
-            'country': 'Country (optional)',
-            'province': 'Province (optional)',
-            'ZIP': 'ZIP (optional)',
-            'fax_number': 'Fax number (optional)',
-            'web_page': 'Web page (optional)',
-            'notes': 'Notes (optional)',
+            'first_name': _('First Name'),
+            'last_name': _('Last Name'),
+            'phone_number': _('Phone number'),
+            'personal_phone_number': _('Personal phone number (optional)'),
+            'email': _('Email'),
+            'job_title': _('Job title'),
+            'address': _('Address (optional)'),
+            'city': _('City (optional)'),
+            'country': _('Country (optional)'),
+            'province': _('Province (optional)'),
+            'ZIP': _('ZIP (optional)'),
+            'fax_number': _('Fax number (optional)'),
+            'web_page': _('Web page (optional)'),
+            'notes': _('Notes (optional)'),
         }
 
         exclude = ['added_at']
@@ -27,15 +33,20 @@ class EmployeeUpdateForm(forms.ModelForm):
         fields = '__all__'
 
         labels = {
-            'personal_phone_number': 'Personal phone number (optional)',
-            'address': 'Address (optional)',
-            'city': 'City (optional)',
-            'country': 'Country (optional)',
-            'province': 'Province (optional)',
-            'ZIP': 'ZIP (optional)',
-            'fax_number': 'Fax number (optional)',
-            'web_page': 'Web page (optional)',
-            'notes': 'Notes (optional)',
+            'first_name': _('First Name'),
+            'last_name': _('Last Name'),
+            'phone_number': _('Phone number'),
+            'personal_phone_number': _('Personal phone number (optional)'),
+            'email': _('Email'),
+            'job_title': _('Job title'),
+            'address': _('Address (optional)'),
+            'city': _('City (optional)'),
+            'country': _('Country (optional)'),
+            'province': _('Province (optional)'),
+            'ZIP': _('ZIP (optional)'),
+            'fax_number': _('Fax number (optional)'),
+            'web_page': _('Web page (optional)'),
+            'notes': _('Notes (optional)'),
         }
 
         exclude = ['added_at']
