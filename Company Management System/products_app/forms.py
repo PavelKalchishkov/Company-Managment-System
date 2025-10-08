422998d8a53f5e719306bc0c39d0b785f394bddb
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-10-08 17:15:52 +0300

    added translation to products
    added translation to products

--- Company Management System/products_app/forms.py
+++ Company Management System/products_app/forms.py
@@ -1,13 +1,32 @@
 from django import forms
-
 from .models import Product
+from django.utils.translation import gettext_lazy as _
+
 
 class ProductCreateForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'length', 'weight', 'color', 'vendor']
 
+        labels = {
+            'name': _('name'),
+            'price': _('price'),
+            'length': _('length'),
+            'weight': _('weight'),
+            'color': _('color'),
+            'vendor': _('vendor')
+        }
+
 class ProductUpdateForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'length', 'weight', 'color', 'vendor']
+
+        labels = {
+            'name': _('name'),
+            'price': _('price'),
+            'length': _('length'),
+            'weight': _('weight'),
+            'color': _('color'),
+            'vendor': _('vendor')
+        }
