185a35c7607a8b3fa3bc0be29a1402e439970370
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-10-09 15:59:01 +0300

    removed staticfiles folder(for deployment)
    removed staticfiles folder(for deployment)

--- Company Management System/staticfiles/javascript/invoice_js/load_newly_created_company.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const companyId = urlParams.get('company');
-    if (companyId) {
-        $('#id_company').val(companyId).trigger('change');
-    }
-});
