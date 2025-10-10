9f1e9bde6229c04d85b77b30200b12c591136677
Pavel Kalchishkov <117100876+PavelKalchishkov@users.noreply.github.com>
2025-10-10 14:17:08 +0300

    added buttons for switching between languages in the home page
    added buttons for switching between languages in the home page

--- Company Management System/diplomna_rabota/urls.py
+++ Company Management System/diplomna_rabota/urls.py
@@ -5,7 +5,7 @@ from .views import custom_permission_denied_view
 from django.conf.urls.i18n import i18n_patterns
 
 
-urlpatterns = []
+urlpatterns = [path('i18n/', include('django.conf.urls.i18n')),]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls, name='admin'),
